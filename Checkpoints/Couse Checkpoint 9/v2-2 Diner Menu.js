585b87fd3136406186965082769da06892e9d803
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2024-04-09 11:48:26 +0200

    CheckPoint 9 updated 09/04/24 am

--- Checkpoints/Couse Checkpoint 9/v2-2 Diner Menu.js
+++ Checkpoints/Couse Checkpoint 9/v2-2 Diner Menu.js
@@ -8,26 +8,39 @@ const menuType = ["Breakfast","Lunch","Dinner"];
 // position 4 = sides prices
 // position 5 = sides comments
 const menuBreakfast = [
-        ["Coffee with milk", "Chocolate"],      // main breakfast description
-        [2, 1.5],                               // main prices
-        ["Good coffee!!", "Sweat drink!!"],     // main comments
-        ["Croassant", "Creps"],                 // sides breakfast description
-        [1.5, 3],                               // sides prices
-        ["It's so crunchy", "Tastes very good"] // sides comments
+        ["Coffee with milk", "Chocolate", "Smoothie"],                  // main breakfast description
+        [2, 1.5, 3],                                                    // main prices
+        ["Good coffee!!", "Sweat drink!!", "Looks very good"],          // main comments
+        ["Croassant", "Creps", "Toast with strawberry jam"],            // sides breakfast description
+        [1.5, 3, 4],                                                    // sides prices
+        ["It's so crunchy", "Tastes very good", "Good choice"]          // sides comments
 ];
-const menuLunchDinner = [                       // lunch and dinner uses same items
-        ["Spagetti carbonara", "Green salad"],  // main lunch/Dinner despcription
-        [7, 5],                                 // main prices
-        ["Molto benne..", "Healthy choise!!"],  // main comments
-        ["Fries", "Fish"],                      // sides lunch/Dinner description
-        [6 , 8],                                // sides prices
-        ["Good choise!!","From the sea to the table !!"] // sides comments
+const menuLunchDinner = [                                               // lunch and diner uses same items
+        ["Spagetti carbonara", "Green salad", "Rice three delicious"],  // main lunch/Dinner despcription
+        [7, 5, 4],                                                      // main prices
+        ["Molto benne..", "Healthy choise!!", "Delicious!!"],           // main comments
+        ["Fries", "Fish", "Grilled rib"],                               // sides lunch/Dinner description
+        [6 , 8, 9],                                                     // sides prices
+        ["Good choise!!","From the sea to the table !!", "Best ones in the world"] // sides comments
 ]
 const arraySelections = [], arrayBill = [], arrayBillPrinting = [];
+const arrayBreakfastTimeRange = [
+        "06:00",
+        "11:59"
+]
+const arrayLunchTimeRange = [
+        "12:00",
+        "18:59"
+]
+const arrayDinnerTimeRange = [
+        "19:00",
+        "23:59"
+]
 var txtInitialMsg = `Welcome to our `, txtInitialMsg2 = `\u03A9 Restaurant XxX \n`;
-var menuTypeSelect = 0, menuMainSelect = 0, menuSideSelect = 0;
+var menuTypeSelect = -1, menuMainSelect = -1, menuSideSelect = -1;
 var dinnerAdditionalCost = 2, totalCost = 0, position = 0;
 var longitudMainSelect = 0;
+var localTime = "";
 /**
  * Options string generator with/without index, from menu array passed
  * @param {array} array
@@ -77,11 +90,10 @@ const stringCommentPrice = (typeSelect, menuMainSideChoice, mainOrSide) => {
 }
 /**
 /* To create Bill string for main/side and price selected 
- * @param {number} typeSelect - if used for Breakfast or LunchDinner
- * @param {number} menuMainSideChoice - chose in main/side prompt
+ * @param {number} typeSelected - if used for Breakfast or LunchDinner
  * @param {string} mainOrSide - indicates if used for main or side
+ * @param {number} menuMainSideChoice - chose in main/side prompt
  */
-
 const lineBillCreator = (typeSelected, mainOrSide, menuMainSideChoice) => {
         // positions selection for menuBreakfast & menuLunchDinner array items
         // depending from mainOrSide
@@ -98,56 +110,125 @@ const lineBillCreator = (typeSelected, mainOrSide, menuMainSideChoice) => {
                                                                         ? 0
                                                                         : dinnerAdditionalCost )
                 }$`;
+}
+/**
+/*
+ * Entrance data validation
+ * @param {string} typeSelection - typed data to validate
+ * @param {string} mainOrSide - calculate length for main or Side array for validation
+ */
+const dataValidator = (typeSelection, mainOrSide) => {
+        var errorMsg = "Invalid selected Option. Pls try again";
+        if (typeSelection == "" || typeSelection < 0) { // no se introduce nada y se da ENTER directamente
+                alert(errorMsg);
+                return true;
+        }
+        if (typeSelection == 0) {// if typed option 0 (is good) then false
+                return false;
+        }
+        if (menuType == 0) { // overrange option check, if good then false, IT USES GLOBAL menuType variable
+                if ((mainOrSide == "main" && (typeSelection <= menuBreakfast[0].length-1)) || (mainOrSide == "side" && (typeSelection <= menuBreakfast[3].length-1)))
+                        return false;
+        }
+        else if ((mainOrSide == "main" && (typeSelection <= menuLunchDinner[0].length-1)) || (mainOrSide == "side" && (typeSelection <= menuLunchDinner[3].length-1)))
+                return false;
+               
+        alert(errorMsg);
+        return true;
+}
+/**
+ * Local Time validator, modify global variable menuTypeSelect to set menu type for the rest of the app
+ * @param {string} - typed local time to validate
+ */
+const localTimeValidator = (timeToValidate) =>{
+        var hourToValidate = timeToValidate.slice(0,timeToValidate.indexOf(":"));
+        var minutesToValidate =timeToValidate.slice(timeToValidate.indexOf(":")+1);
+        var errorMsg = "Invalid time, pls try again";
+        if (hourToValidate == "" || minutesToValidate == "") { // no se introduce nada y se da ENTER directamente
+                alert(errorMsg);
+                return true;
+        }
+/*        if (hourToValidate >= 0 && hourToValidate < 24 && minutesToValidate >= 0 && minutesToValidate < 60) {
+                if (timeToValidate >= "06:00" &&  timeToValidate <= "11:59"){
+                        menuTypeSelect = 0; } // breakfast;
+                else if (timeToValidate >= "12:00" &&  timeToValidate <= "17:59") {
+                        menuTypeSelect = 1; } // lunch;
+                else if (timeToValidate >= "18:00" &&  timeToValidate <= "23:59") {
+                        menuTypeSelect = 2; } // diner;
+                else {
+                        alert(" Sorry but, the restaurant is closed ...");
+                        logout();
+                }
+                return false;
+        }*/
+        if (hourToValidate >= 0 && hourToValidate < 24 && minutesToValidate >= 0 && minutesToValidate < 60) {
+                if (timeToValidate >= arrayBreakfastTimeRange[0] &&  timeToValidate <= arrayBreakfastTimeRange[1]){
+                        menuTypeSelect = 0; } // breakfast;
+                else if (timeToValidate >= arrayLunchTimeRange[0] &&  timeToValidate <= arrayLunchTimeRange[1]) {
+                        menuTypeSelect = 1; } // lunch;
+                else if (timeToValidate >= arrayDinnerTimeRange[0] &&  timeToValidate <= arrayDinnerTimeRange[1]) {
+                        menuTypeSelect = 2; } // diner;
+                else {
+                        alert(" Sorry but, the restaurant is closed ...");
+                        logout();
+                }
+                return false;
+        }
 
+        alert(errorMsg);
+        return true;
 }
 // *********************
 // Main program starting
 // *********************
-alert(txtInitialMsg+txtInitialMsg2);
-do {    //Menu type selection (Breakfast or LunchDinner), stored in menuTypeSelect and USED
-        // IN A LOT OF CODE to indicate array Breakfast or LunchDinner to be used
-        menuTypeSelect = prompt(`Please select menu type:\n${stringOptionsGenerator(menuType, true)}`);
-} while (menuTypeSelect < 0 || menuTypeSelect >= menuType.length || menuTypeSelect === "");
-// Storing menu type breakfast or lunch/dinner in array for using later for bill printing
+//alert(txtInitialMsg+txtInitialMsg2);
+// Menu type selection (Breakfast or LunchDinner), stored in menuTypeSelect and USED
+// IN A LOT OF CODE to indicate array Breakfast or LunchDinner to be used
+
+do {
+        localTime = prompt(`${txtInitialMsg+txtInitialMsg2}\n\n`+`Please, type local time (hh:mm 24h format)`);
+} while (localTimeValidator(localTime));
+//Menu type selection (Breakfast or LunchDinner), stored in menuTypeSelect and USED
+// IN A LOT OF CODE to indicate array Breakfast or LunchDinner to be used
+
+// Storing menu type breakfast or lunch/diner in array for using later for bill printing
 arraySelections.push(menuTypeSelect);
-// Showing main available options for menu type selected, 
-// First parameter menuTypeSelect (breakfast or lunch/dinner)
+
+// Showing main available options for menu type selected,  using stringOptionsGeneratorWrapper
+// First parameter menuTypeSelect (breakfast or lunch/diner)
 // as second parameter position for main or side in appropiate array (menuBreakfast or menuLunchDinner)
-alert(`Selected: ${menuType [menuTypeSelect]}\n`+
+alert(`It's time for: ${menuType [menuTypeSelect]}\n`+
         `Today we have:\nFor Main:`+
         `${stringOptionsGeneratorWrapper(menuTypeSelect , 0)}`+
         `\nFor Sides:`+
         `${stringOptionsGeneratorWrapper(menuTypeSelect , 3)}`+
         `\n`);
 do {    // Main selection, stored in menuMainSelect
-        // Next line could be improved in order to don't repeat code but
-        // due to still I don't know how to return 2 values from function,
-        // I expect to learn how to do it in a near future...
         menuMainSelect = prompt(`Please select Main:\n`+
                 `${menuTypeSelect == 0 
-                        ? (longitudMainSelect = menuBreakfast[0].length,
-                        stringOptionsGenerator(menuBreakfast[0], true))
-                        : (longitudMainSelect = menuLunchDinner[0].length,
-                        stringOptionsGenerator(menuLunchDinner[0], true))
+                        ? stringOptionsGenerator(menuBreakfast[0], true)
+                        : stringOptionsGenerator(menuLunchDinner[0], true)
                 }`);
-} while (menuMainSelect < 0 || menuMainSelect >= longitudMainSelect || menuMainSelect === "");
+} while (dataValidator(menuMainSelect, "main"));
+
 // Storing main in array for using later for bill printing
 arraySelections.push(menuMainSelect);
+
 // Showing comment and price for main selected
 alert(`${stringCommentPrice(menuTypeSelect, menuMainSelect, "main")}`);
 do {    //Side selection, stored in menuSideSelect
         menuSideSelect = prompt(`Please select Side:\n`+
                 `${menuTypeSelect == 0 
-                        ? (longitudMainSelect = menuBreakfast[3].length,
-                        stringOptionsGenerator(menuBreakfast[3], true))
-                        : (longitudMainSelect = menuLunchDinner[3].length,
-                        stringOptionsGenerator(menuLunchDinner[3], true))
-                }`);
-} while (menuSideSelect < 0 || menuSideSelect >= longitudMainSelect || menuSideSelect === "");
+                        ? stringOptionsGenerator(menuBreakfast[3], true)
+                        : stringOptionsGenerator(menuLunchDinner[3], true)
+                }`);                
+} while (dataValidator(menuSideSelect, "side"));
+
 // Storing side in array for using later for bill printing
 arraySelections.push(menuSideSelect);
 // Showing comment and price for side selected
 alert(`${stringCommentPrice(menuTypeSelect, menuSideSelect, "side")}`);
+
 //
 // Creating Bill lines ant storing them in arrayBill
 //
@@ -180,5 +261,5 @@ alert(arrayBillPrinting);
 // The end ... Even if this code it's right or wrong, I enjoyed typing this code
 // Pls, any comments will be highly appreciated, it is for sure this exercice could be 
 // done by another way
-// Pending menu maintenance options (create, modifications, deletions, ...)
+// Pending menu maintenance options (create, modificate, delete, ...)
 // See you soon ...
