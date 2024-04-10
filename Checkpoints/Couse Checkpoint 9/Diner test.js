89c0b71ceef54e04a79bae5cc6c8908259a98450
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2024-04-10 22:37:52 +0200

    Unhiding test.html

---
+++ Checkpoints/Couse Checkpoint 9/Diner test.js
@@ -0,0 +1,17 @@
+menus = [
+    {menu: ["Breakfast"],
+     main: ["Coffee with milk", "Chocolate", "Smoothie"],
+     mainPrices: [2, 1.5, 3], 
+     mainComments: ["Good coffee!!", "Sweat drink!!", "Looks very good"],
+     sides: ["Croassant", "Creps", "Toast with strawberry jam"],
+     sidesPrices: [1.5, 3, 4],
+     sidesComments: ["It's so crunchy", "Tastes very good", "Good choice"] },
+
+    {menu: ["Lunch", "Diner"],
+     main: ["Spagetti carbonara", "Green salad", "Rice three delicious"],
+     mainPrices: [7, 5, 4], 
+     mainComments: ["Molto benne..", "Healthy choise!!", "Delicious!!"],
+     sides: ["Fries", "Fish", "Grilled rib"],
+     sidesPrices: [6 , 8, 9],
+     sidesComments: ["Good choise!!","From the sea to the table !!", "Best ones in the world"]}
+]
