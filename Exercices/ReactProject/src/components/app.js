64508c817af88fc69e88b43d397a43e179548d67
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2024-05-08 19:07:34 +0200

    React module instalation

---
+++ Exercices/ReactProject/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>DevCamp React Starter</h1>
+        <h2>React Bootstrap</h2>
+        <h3>Eto e la otia !!!</h3>
+      </div>
+    );
+  }
+}
