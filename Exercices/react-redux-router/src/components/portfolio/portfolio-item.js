63ce8ed416dab56c34ec9bca98345a4e0c70f6a6
JasonRodriguezGarcia <jasonrodriguezempleo.gmail.com>
2024-05-18 17:02:13 +0200

    Added react-portfolio-assets updated portfolio-container and item improving props call

--- Exercices/react-redux-router/src/components/portfolio/portfolio-item.js
+++ Exercices/react-redux-router/src/components/portfolio/portfolio-item.js
@@ -4,12 +4,29 @@ import { Link } from "react-router-dom";
 // less functionality than a class function, it allows to:
 // render content, pass data, images, info ...
 export default function(props) { // props are like parameters
+
+  const {id, description, thumb_image_url, logo } = props.item;
   return (
-    // attending to props title and url came from outside
+    // attending to props title and url came from outside (old version)
+    // <div>
+    //   <h3>{props.title}</h3> 
+    //   <h4>{props.url}</h4>
+    //   <Link to={`/portfolio/${props.slug}`}>Link</Link>
+    // </div>
+
+    // Data we will need:
+    // - background image: thumb_image_url
+    // - logo
+    // - description: description
+    // - id: id
+    
     <div>
-      <h3>{props.title}</h3> 
-      <h4>{props.url}</h4>
-      <Link to={`/portfolio/${props.slug}`}>Link</Link>
+      <div>
+        <img src={thumb_image_url} />
+        <img scr={logo} />
+        {description}
+      </div>
+      <Link to={`/portfolio/${id}`}>Link</Link>
     </div>
   );
 }
